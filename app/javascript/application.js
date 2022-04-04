ecd7386a7efb108cd02307c01629e7327d8223a2
FranciaMMI <francia.bidzimou@mmibordeaux.com>
2022-04-04 14:30:20 +0200

    maj

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import * as bootstrap from "bootstrap"
+import * as bootstrap from "bootstrap";
+import * as mdb from 'mdb-ui-kit'; // lib
+import { Input } from 'mdb-ui-kit'; // module
